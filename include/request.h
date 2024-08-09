e359162263c27287a316b44ec695ce504d530e24
wrfz <wr.github@topofweb.de>
2024-08-09 15:11:13 +0200

    Improve request handling

--- include/request.h
+++ include/request.h
@@ -17,7 +17,8 @@ public:
     , m_response_can_id(response_can_id)
     , m_expected_reponse(expected_reponse)
     , m_lambda(lambda)
-    , m_lambda_used(true)
+    , m_last_update(0u)
+    , m_last_request(0u)
     {
     }
 
@@ -29,46 +30,77 @@ public:
     {
     }
 
-    std::array<uint8_t, 7> data() const
+    void init(uint32_t request_index) {
+        m_request_index = request_index;
+    }
+
+    std::array<uint8_t, 7> const& data() const
     {
         return m_data;
     }
 
-    bool startsWith(std::vector<uint8_t> const& data) const {
-        for (uint32_t index = 0; index < data.size(); ++index) {
-            if (m_expected_reponse[index] != DC && data[index] != m_expected_reponse[index]) {
-                return false;
-            }
-        }
-        return true;
+    uint32_t getLastUpdate() const {
+        return m_last_update;
     }
 
-    bool handle(uint32_t can_id, std::vector<uint8_t> const& data) const {
-        if (m_lambda_used && can_id == m_response_can_id && startsWith(data)) {
-            m_lambda(data);
+    bool isMatch(uint32_t can_id, std::vector<uint8_t> const& responseData) const {
+        if (can_id == m_response_can_id) {
+            for (uint32_t index = 0; index < responseData.size(); ++index) {
+                if (m_expected_reponse[index] != DC && responseData[index] != m_expected_reponse[index]) {
+                    return false;
+                }
+            }
             return true;
         }
         return false;
     }
 
+    bool handle(uint32_t can_id, std::vector<uint8_t> const& responseData, uint32_t timestamp) {
+        if (isMatch(can_id, responseData)) {
+            m_lambda(responseData);
+            m_last_update = timestamp;
+            return true;
+        }
+        return false;
+    }
+
+    void send(esphome::esp32_can::ESP32Can* pCanBus) {
+        const uint32_t can_id = 0x680;
+        const bool use_extended_id = false;
+
+        pCanBus->send_data(can_id, use_extended_id, { m_data.begin(), m_data.end() });
+        ESP_LOG_FILTER("main", "request(%d, %s, %s)", m_request_index, getName().c_str(), to_hex(m_data).c_str());
+        m_last_request = millis();
+    }
+
     std::string const& getName() const {
         return m_name;
     }
+
+    bool inProgress() const {
+        return m_last_request > m_last_update;
+    }
 private:
+    uint32_t m_request_index;
     std::string m_name;
     std::array<uint8_t, 7> m_data;
     uint32_t m_response_can_id;
     std::array<uint16_t, 7> m_expected_reponse;
     std::function<void(std::vector<uint8_t> const&)> m_lambda;
-    bool m_lambda_used;
+    uint32_t m_last_update;
+    uint32_t m_last_request;
 };
 
 class TRequests {
 public:
     TRequests(std::vector<TRequest> const& requests)
     : m_requests(requests)
-    , request_index(0u)
+    , m_update_interval(1000u)
     {
+        uint32_t index = 0;
+        for (TRequest& request : m_requests) {
+            request.init(index++);
+        }
     }
 
     uint32_t size() const {
@@ -79,36 +111,59 @@ public:
         return m_requests[index];
     }
 
-    bool handle(esphome::esp32_can::ESP32Can* can_bus, uint32_t can_id, std::vector<uint8_t> const& data, uint32_t timestamp) const {
-        ESP_LOG_FILTER("handle", "can_id<%s> data<%s>", to_hex(can_id).c_str(), to_hex(data).c_str());
-        for (auto const& request : m_requests) {
-            if (request.handle(can_id, data)) {
-                return true;
-            }
+    void setUpdateInterval(uint32_t update_interval) {
+        m_update_interval = update_interval;
+    }
+
+    void handle(esphome::esp32_can::ESP32Can* pCanBus, uint32_t can_id, std::vector<uint8_t> const& responseData, uint32_t timestamp) {
+        ESP_LOG_FILTER("handle", "can_id<%s> responseData<%s>", to_hex(can_id).c_str(), to_hex(responseData).c_str());
+        TRequest& request = findRequest(can_id, responseData);
+        request.handle(can_id, responseData, timestamp);
+
+        sendNextPendingRequest(pCanBus);
+    }
+
+    bool sendNextPendingRequest(esphome::esp32_can::ESP32Can* pCanBus)
+    {
+        TRequest* pRequest = getNextRequestToSend();
+        if (pRequest != nullptr) {
+            pRequest->send(pCanBus);
+            return true;
         }
         return false;
     }
 
-    void sendRequest(esphome::esp32_can::ESP32Can* can_bus, uint32_t message_index = 0xFFFFFFFF)
+    void sendRequest(esphome::esp32_can::ESP32Can* pCanBus, uint32_t message_index)
     {
-        if (message_index != 0xFFFFFFFF) {
-            request_index = message_index;
+        if (message_index >= 0 && message_index < m_requests.size()) {
+            TRequest& request = m_requests[message_index];
+            request.send(pCanBus);
         }
-        if (request_index >= size()) {
-            request_index = 0;
-        }
-
-        TRequest const& request = get(request_index);
-        const uint32_t can_id = 0x680;
-        const bool use_extended_id = false;
-        std::array<uint8_t, 7> const& data = request.data();
-
-        can_bus->send_data(can_id, use_extended_id, { data.begin(), data.end() });
-        ESP_LOG_FILTER("main", "request(%d, %s, %s)", request_index++, request.getName().c_str(), to_hex(request.data()).c_str());
     }
+
 private:
+    TRequest& findRequest(uint32_t can_id, std::vector<uint8_t> const& responseData) {
+        for (auto& request : m_requests) {
+            if (request.isMatch(can_id, responseData)) {
+                return request;
+            }
+        }
+        return m_requests[0];   // should never happen!
+    }
+
+    TRequest* getNextRequestToSend() {
+        const uint32_t timestamp = millis();
+
+        for (auto& request : m_requests) {
+            if ((timestamp > request.getLastUpdate() + m_update_interval) && !request.inProgress()) {
+                return &request;
+            }
+        }
+        return nullptr;
+    }
+
     std::vector<TRequest> m_requests;
-    uint32_t request_index;
+    uint32_t m_update_interval;
 };
 
 
