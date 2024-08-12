7533a0f8909642e6936774ef9d80226461da2983
wrfz <wr.github@topofweb.de>
2024-08-12 14:36:40 +0200

    Add sendSet method

--- include/HPSU_compact_508.h
+++ include/HPSU_compact_508.h
@@ -29,7 +29,7 @@ static const BidiMap<uint8_t, std::string> map_sg = {
 };
 
 const std::vector<TRequest> entity_config = {
-    {// 0
+    {
         "Status Kessel",
         {0x31, 0x00, 0xFA, 0x0A, 0x8C, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0x0A, 0x8C,   DC,   DC},
@@ -39,7 +39,7 @@ const std::vector<TRequest> entity_config = {
             return status;
         }
     },
-    {// 1
+    {
         "Umwälzpumpe",
         {0x31, 0x00, 0xFA, 0xC0, 0xF7, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0xC0, 0xF7,   DC,   DC},
@@ -49,17 +49,16 @@ const std::vector<TRequest> entity_config = {
             return status;
         }
     },
-    {// 2
+    {
         "Betriebsmodus",
         {0x31, 0x00, 0xFA, 0x01, 0x12, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0x01, 0x12,   DC,   DC},
         [](auto const& data) -> DataType {
             id(Betriebsmodus).publish_state(data[5]);
-            const std::string mode = Utils::setSelectOption(id(select_betriebsmodus), map_betriebsmodus, data[5]);
-            return mode;
+            return Utils::setSelectOption(id(select_betriebsmodus), map_betriebsmodus, data[5]);
         }
     },
-    {// 3
+    {
         "Betriebsart",
         {0x31, 0x00, 0xFA, 0xC0, 0xF6, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0xC0, 0xF6,   DC,   DC},
@@ -69,7 +68,7 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 4
+    {
         "VL Soll",
         {0x31, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00},
         {0xD2,   DC, 0x02,   DC,   DC,   DC,   DC},
@@ -79,7 +78,7 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 5
+    {
         "BPV",
         {0x31, 0x00, 0xFA, 0xC0, 0xFB, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0xC0, 0xFB,   DC,   DC},
@@ -89,7 +88,7 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 6
+    {
         "Durchfluss",
         {0x31, 0x00, 0xFA, 0x01, 0xDA, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0x01, 0xDA,   DC,   DC},
@@ -99,7 +98,7 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 7
+    {
         "Laufzeit Pumpe",
         {0x31, 0x00, 0xFA, 0x06, 0xA4, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0x06, 0xA4,   DC,   DC},
@@ -109,7 +108,7 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 8
+    {
         "Vorlauftemperatur (TVBH)",
         {0x31, 0x00, 0xFA, 0xC1, 0x02, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0xC1, 0x02,   DC,   DC},
@@ -119,7 +118,7 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 9
+    {
         "Vorlauftemperatur",
         {0x31, 0x00, 0xFA, 0xC0, 0xFC, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0xC0, 0xFC,   DC,   DC},
@@ -129,7 +128,7 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 10
+    {
         "Rücklauftemperatur Heizung",
         {0x31, 0x00, 0xFA, 0xC1, 0x00, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0xC1, 0x00,   DC,   DC},
@@ -139,7 +138,7 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 11
+    {
         "Warmwassertemperatur",
         {0x31, 0x00, 0xFA, 0x00, 0x0E, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0x00, 0x0E,   DC,   DC},
@@ -149,7 +148,7 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 12
+    {
         "Warmwasser soll",
         {0x31, 0x00, 0x13, 0x00, 0x00, 0x00, 0x00},
         {0xD2, 0x00, 0x13,   DC,   DC, 0x00, 0x00},
@@ -157,10 +156,11 @@ const std::vector<TRequest> entity_config = {
             uint16_t temperature_raw = (data[3] <<  8) | data[4]; // Convert to int16be
             float temperature = static_cast<float>(temperature_raw) / 10.0;
             id(t_ww_soll).publish_state(temperature);
+            id(ww_soll).publish_state(temperature);
             return temperature;
         }
     },
-    {// 13
+    {
         "Raumsoll 1",
         {0x31, 0x00, 0x05, 0x00, 0x00, 0x00, 0x00},
         {0xD2, 0x00, 0x05, 0x00,   DC, 0x00,   DC},
@@ -170,7 +170,7 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 14
+    {
         "Laufzeit Kompressor",
         {0x31, 0x00, 0xFA, 0x06, 0xA5, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0x06, 0xA5,   DC,   DC},
@@ -180,7 +180,7 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 15
+    {
         "Min Vl Soll",
         {0x31, 0x00, 0xFA, 0x01, 0x2B, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0x01, 0x2B,   DC,   DC},
@@ -190,7 +190,7 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 16
+    {
         "Max Vl Soll",
         {0x31, 0x00, 0x28, 0x00, 0x00, 0x00, 0x00},
         {  DC,   DC, 0x28,   DC,   DC,   DC,   DC},
@@ -200,17 +200,16 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 17
+    {
         "HK Funktion",
         {0x31, 0x00, 0xFA, 0x01, 0x41, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0x01, 0x41,   DC,   DC},
         [](auto const& data) -> DataType {
             id(hk_funktion).publish_state(data[6]);
-            const std::string mode = Utils::setSelectOption(id(select_hk), map_hk, data[6]);
-            return mode;
+            return Utils::setSelectOption(id(select_hk), map_hk, data[6]);
         }
     },
-    {// 18
+    {
         "Temperatur Vorlauf Tag",
         {0x31, 0x00, 0xFA, 0x01, 0x29, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0x01, 0x29,   DC,   DC},
@@ -220,7 +219,7 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 19
+    {
         "Fehlercode",
         {0x31, 0x00, 0xFA, 0x13, 0x88, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0x13, 0x88,   DC,   DC},
@@ -230,7 +229,7 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 20
+    {
         "Erzeugte Energie gesamt",
         {0x31, 0x00, 0xFA, 0x09, 0x30, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0x09, 0x30,   DC,   DC},
@@ -240,7 +239,7 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 21
+    {
         "Wasserdruck",
         {0x31, 0x00, 0x1C, 0x00, 0x00, 0x00, 0x00},
         {0xD2,   DC, 0x1C,   DC,   DC,   DC,   DC},
@@ -250,7 +249,7 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 22
+    {
         "EHS für CH",
         {0x31, 0x00, 0xFA, 0x09, 0x20, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0x09, 0x20,   DC,   DC},
@@ -260,8 +259,8 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 23
-        "Außentemperatur",
+    {
+        "Aussentemperatur",
         {0x31, 0x00, 0xFA, 0xC0, 0xFF, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0xC0, 0xFF,   DC,   DC},
         [](auto const& data) -> DataType {
@@ -270,18 +269,34 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 24
+    {
         "Heizkurve",
         {0x31, 0x00, 0xFA, 0x01, 0x0E, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0x01, 0x0E,   DC,   DC},
         [](auto const& data) -> DataType {
             const float value = (uint32_t(data[6]) + (data[5] << 8)) / 100.0f;
             id(heizkurve).publish_state(value);
-            id(set_heizkurve).publish_state(value);
             return value;
         }
     },
-    {// 25
+    {
+        "Heizkurve Einstellen",
+        {0x31, 0x00, 0xFA, 0x01, 0x0E, 0x00, 0x00},
+        {  DC,   DC, 0xFA, 0x01, 0x0E,   DC,   DC},
+        [](auto const& data) -> DataType {
+            const float value = (uint32_t(data[6]) + (data[5] << 8)) / 100.0f;
+            id(set_heizkurve).publish_state(value);
+            return value;
+        },
+        [](auto const& value) -> std::vector<uint8_t> {
+            const uint16_t hk = (uint16_t)(value * 100);
+            const uint8_t high_byte = hk >> 8;
+            const uint8_t low_byte = hk & 0xFF;
+
+            return { 0x30, 0x00, 0xFA, 0x01, 0x0E, high_byte, low_byte };
+        }
+    },
+    {
         "Spreizung MOD HZ",
         {0x31, 0x00, 0xFA, 0x06, 0x83, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0x06, 0x83,   DC,   DC},
@@ -291,7 +306,7 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 26
+    {
         "Spreizung MOD WW",
         {0x31, 0x00, 0xFA, 0x06, 0x84, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0x06, 0x84,   DC,   DC},
@@ -301,27 +316,25 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 27
+    {
         "SGModus",
         {0x31, 0x00, 0xFA, 0x06, 0x94, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0x06, 0x94,   DC,   DC},
         [](auto const& data) -> DataType {
             id(SGModus).publish_state(data[6]);
-            const std::string mode = Utils::setSelectOption(id(select_sg_mode), map_sg_mode, data[6]);
-            return mode;
+            return Utils::setSelectOption(id(select_sg_mode), map_sg_mode, data[6]);
         }
     },
-    {// 28
+    {
         "Smart Grid",
         {0x31, 0x00, 0xFA, 0x06, 0x93, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0x06, 0x93,   DC,   DC},
         [](auto const& data) -> DataType {
             id(Smartgrid).publish_state(data[6]);
-            const std::string mode = Utils::setSelectOption(id(select_smartgrid), map_sg, data[6]);
-            return mode;
+            return Utils::setSelectOption(id(select_smartgrid), map_sg, data[6]);
         }
     },
-    {// 29
+    {
         "Status Kompressor",
         {0xA1, 0x00, 0x61, 0x00, 0x00, 0x00, 0x00},
         0x500,
@@ -332,7 +345,7 @@ const std::vector<TRequest> entity_config = {
             return temperature;
         }
     },
-    {// 30
+    {
         "Status Mischer DHW",
         {0x31, 0x00, 0xFA, 0x06, 0x9B, 0x00, 0x00},
         {  DC,   DC, 0xFA, 0x06, 0x9B,   DC,   DC},
@@ -342,7 +355,7 @@ const std::vector<TRequest> entity_config = {
             return status;
         }
     },
-    {// 31
+    {
         "Heizstabrelais",
         {0x31, 0x00, 0xFA, 0x0A, 0x20, 0x00, 0x00},
         {0xD2, 0x00, 0xFA, 0x0A, 0x20, DC, DC},
@@ -353,7 +366,7 @@ const std::vector<TRequest> entity_config = {
             id(status_heizstab).publish_state((0x4 * r1) | (0x2 * r2) | r3);
             return Utils::format("Heizstabrelais: %d|%d|%d status: %d", r1, r2, r3, status_heizstab);
         }
-    },  // Status Heizstäbe
+    },
     /*{// 32
         "zzz",
         {0x31, 0x00, 0x0E, 0x00, 0x00, 0x00, 0x00},
--- include/request.h
+++ include/request.h
@@ -6,17 +6,21 @@ const uint16_t DC = 0xFFFF; // Don't care
 
 class TRequest
 {
+    using TGetLambda = std::function<DataType(std::vector<uint8_t> const&)>;
+    using TSetLambda = std::function<std::vector<uint8_t>(float const&)>;
 public:
     TRequest(std::string const& name,
         std::array<uint8_t, 7> const& data,
         uint32_t response_can_id,
         std::array<uint16_t, 7> const& expected_reponse,
-        std::function<DataType(std::vector<uint8_t> const&)> lambda)
+        TGetLambda lambda,
+        TSetLambda setLambda)
     : m_name(name)
     , m_data(data)
     , m_response_can_id(response_can_id)
     , m_expected_reponse(expected_reponse)
     , m_lambda(lambda)
+    , m_set_lambda(setLambda)
     , m_last_update(0u)
     , m_last_request(0u)
     {
@@ -24,14 +28,32 @@ public:
 
     TRequest(std::string const& name,
         std::array<uint8_t, 7> const& data,
+        uint32_t response_can_id,
         std::array<uint16_t, 7> const& expected_reponse,
-        std::function<DataType(std::vector<uint8_t> const&)> lambda)
+        TGetLambda lambda)
+    : TRequest(name, data, response_can_id, expected_reponse, lambda, [](float) -> std::vector<uint8_t> { return {}; })
+    {
+    }
+
+    TRequest(std::string const& name,
+        std::array<uint8_t, 7> const& data,
+        std::array<uint16_t, 7> const& expected_reponse,
+        TGetLambda lambda,
+        TSetLambda setLambda)
+    : TRequest(name, data, 0x180, expected_reponse, lambda, setLambda)
+    {
+    }
+
+    TRequest(std::string const& name,
+        std::array<uint8_t, 7> const& data,
+        std::array<uint16_t, 7> const& expected_reponse,
+        TGetLambda lambda)
     : TRequest(name, data, 0x180, expected_reponse, lambda)
     {
     }
 
-    void init(uint32_t request_index) {
-        m_request_index = request_index;
+    std::string const& getName() const {
+        return m_name;
     }
 
     uint32_t getLastUpdate() const {
@@ -63,7 +85,7 @@ public:
             } else {
                 value = "Unsupported value type!";
             }
-            Utils::log("handled", "%s<%s> can_id<%s> data<%s>",
+            Utils::log("request.h", "handled: %s<%s> can_id<%s> data<%s>",
                 m_name.c_str(), value.c_str(), Utils::to_hex(can_id).c_str(), Utils::to_hex(responseData).c_str());
 
             m_last_update = timestamp;
@@ -72,14 +94,28 @@ public:
         return false;
     }
 
-    void request(esphome::esp32_can::ESP32Can* pCanBus) {
+    void sendGet(esphome::esp32_can::ESP32Can* pCanBus) {
         const uint32_t can_id = 0x680;
         const bool use_extended_id = false;
 
         pCanBus->send_data(can_id, use_extended_id, { m_data.begin(), m_data.end() });
-        Utils::log("request", "can_id<%d> name<%s> data<%s>", m_request_index, m_name.c_str(), Utils::to_hex(m_data).c_str());
+        Utils::log("request.h", "sendGet: name<%s> can_id<%s> data<%s>",
+            m_name.c_str(), Utils::to_hex(can_id).c_str(), Utils::to_hex(m_data).c_str());
 
         m_last_request = millis();
+    }
+
+    void sendSet(esphome::esp32_can::ESP32Can* pCanBus, float value) {
+        const uint32_t can_id = 0x680;
+        const bool use_extended_id = false;
+
+        auto data = m_set_lambda(value);
+
+        pCanBus->send_data(can_id, use_extended_id, { data.begin(), data.end() });
+        Utils::log("request.h", "sendSet: name<%s> value<%f> can_id<%s> data<%s>",
+            m_name.c_str(), value, Utils::to_hex(can_id).c_str(), Utils::to_hex(data).c_str());
+
+        sendGet(pCanBus);
     }
 
     bool inProgress() const {
@@ -87,12 +123,12 @@ public:
     }
 
 private:
-    uint32_t m_request_index;
     std::string m_name;
     std::array<uint8_t, 7> m_data;
     uint32_t m_response_can_id;
     std::array<uint16_t, 7> m_expected_reponse;
-    std::function<DataType(std::vector<uint8_t> const&)> m_lambda;
+    TGetLambda m_lambda;
+    std::function<std::vector<uint8_t>(float const&)> m_set_lambda;
     uint32_t m_last_update;
     uint32_t m_last_request;
 };
@@ -102,10 +138,6 @@ public:
     TRequests(std::vector<TRequest> const& requests)
     : m_requests(requests)
     {
-        uint32_t index = 0;
-        for (TRequest& request : m_requests) {
-            request.init(index++);
-        }
     }
 
     uint32_t size() const {
@@ -117,40 +149,50 @@ public:
     }
 
     void handle(esphome::esp32_can::ESP32Can* pCanBus, uint32_t can_id, std::vector<uint8_t> const& responseData, uint32_t timestamp) {
-        TRequest* pRequest = findRequest(can_id, responseData);
-        if (pRequest != nullptr) {
-            pRequest->handle(can_id, responseData, timestamp);
-        } else {
-            Utils::log("unhandled", "can_id<%s> data<%s>", Utils::to_hex(can_id).c_str(), Utils::to_hex(responseData).c_str());
+        bool bHandled = false;
+        for (auto& request : m_requests) {
+            if (request.isMatch(can_id, responseData)) {
+                request.handle(can_id, responseData, timestamp);
+                bHandled = true;
+            }
+        }
+        if (!bHandled) {
+            Utils::log("request.h", "unhandled: can_id<%s> data<%s>", Utils::to_hex(can_id).c_str(), Utils::to_hex(responseData).c_str());
         }
     }
 
-    bool sendNextPendingRequest(esphome::esp32_can::ESP32Can* pCanBus) {
+    bool sendNextPendingGet(esphome::esp32_can::ESP32Can* pCanBus) {
         TRequest* pRequest = getNextRequestToSend();
         if (pRequest != nullptr) {
-            pRequest->request(pCanBus);
+            pRequest->sendGet(pCanBus);
             return true;
         }
         return false;
     }
 
-    void sendRequest(esphome::esp32_can::ESP32Can* pCanBus, uint32_t message_index) {
-        if (message_index >= 0 && message_index < m_requests.size()) {
-            TRequest& request = m_requests[message_index];
-            request.request(pCanBus);
+    void sendGet(esphome::esp32_can::ESP32Can* pCanBus, std::string const& request_name) {
+        const auto it = std::find_if(m_requests.begin(), m_requests.end(),
+            [& request_name](auto& request) { return request.getName() == request_name; });
+
+        if (it != m_requests.end()) {
+            it->sendGet(pCanBus);
+        } else {
+            Utils::log("request.h", "sendGet(%s) -> Unknown request!", request_name.c_str());
+        }
+    }
+
+    void sendSet(esphome::esp32_can::ESP32Can* pCanBus, std::string const& request_name, float value) {
+        const auto it = std::find_if(m_requests.begin(), m_requests.end(),
+            [& request_name](auto& request) { return request.getName() == request_name; });
+
+        if (it != m_requests.end()) {
+            it->sendSet(pCanBus, value);
+        } else {
+            Utils::log("request.h", "sendSet(%s) -> Unknown request!", request_name.c_str());
         }
     }
 
 private:
-    TRequest* findRequest(uint32_t can_id, std::vector<uint8_t> const& responseData) {
-        for (auto& request : m_requests) {
-            if (request.isMatch(can_id, responseData)) {
-                return &request;
-            }
-        }
-        return nullptr;
-    }
-
     TRequest* getNextRequestToSend() {
         const uint32_t timestamp = millis();
         const uint32_t interval = static_cast<uint32_t>(id(update_interval).state) * 1000;
